a9b830315f084bdc4954f454340745a949e6ab7d
xchen100 <407081305@qq.com>
2016-12-17 22:37:57 +0800

    新增今日天气布局

---
+++ app/src/main/java/com/example/xchen/mweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.xchen.mweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xchen on 16/12/17.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
