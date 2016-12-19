972ae6ec311d7886970c122b901591252ec8d212
xchen100 <407081305@qq.com>
2016-12-19 22:23:23 +0800

    修改－城市选择界面：新增返回动作

--- app/src/main/java/com/example/xchen/mweather/SelectCity.java
+++ app/src/main/java/com/example/xchen/mweather/SelectCity.java
@@ -2,15 +2,34 @@ package com.example.xchen.mweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by xchen on 16/12/19.
  */
 
-public class SelectCity extends Activity{
+public class SelectCity extends Activity implements View.OnClickListener{
+    private ImageView backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_city);
+
+        backBtn = (ImageView)findViewById(R.id.title_selectCity_back);
+        backBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.title_selectCity_back:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
